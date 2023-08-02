40107056ab2e65e7d6d2b1f0d149269492ef20ed
André de Araújo Costa Driemeyer <andredriem@gmail.com>
2023-08-02 20:01:41 -0300

    Added state compresser for more efficient data managment

--- src/hook_builder.ts
+++ src/hook_builder.ts
@@ -22,7 +22,11 @@ abstract class Subscriber<Data, ErrorData> {
     this.observersFunctions.set(componentId, callback)
 
     if (Object.entries(this.observersFunctions).length === 1) {
-      void this.fetchData(componentId, this.notifyObservers)
+      this.fetchData(componentId, this.notifyObservers).catch((e) => {
+        console.error(`fetchData function failed with parameters: ${this.parameters}.` +
+          'Components using this parameter will stop having their state updated')
+        console.error(e)
+      })
     }
   }
 
@@ -30,7 +34,11 @@ abstract class Subscriber<Data, ErrorData> {
     this.observersFunctions.delete(componentId)
 
     if (this.observersFunctions.size === 0) {
-      void this.stopFetching(componentId)
+      this.stopFetching(componentId).catch((e) => {
+        console.error(`stopFetching function failed with parameters: ${this.parameters}.` +
+          'This might result in your data still beeing fetched after the component is unmounted')
+        console.error(e)
+      })
     }
   }
 
@@ -43,21 +51,37 @@ abstract class Subscriber<Data, ErrorData> {
 
 /**
  * Generic Hook that fetches data from and subsciber class and manages the current state of the
- * component based on the incoming data
- * @param parameter unique id for the data you are fetching
- * @param subscriberPool pool of subscribers for this hook
- * @param SubcriberClass class that implements the subscriber (the one that is in the pool)
- * @returns [data, error, componentId] Based on your notification function in the subscriber you will receive
- * the data and error in the hook.
+ *
+ * @template Data the type of the data that will be handled.
+ * @template ErrorData the type of the error data that will be handled.
+ * @template CompressedData the type of the data after being compressed by the stateCompresser function.
+ *
+ * @param {string} parameter - Unique id for the data you are fetching.
+ * @param {Record<string, Subscriber<Data, ErrorData> | undefined>} subscriberPool
+ * - Pool of subscribers for this hook.
+ * @param {new (parameters: string) => Subscriber<Data, ErrorData>} SubcriberClass
+ * - Class that implements the subscriber (the one that is in the pool).
+ * @param {(data: Data | null) => CompressedData | null} stateCompresser
+ * - Function that will reduce the data to the state if you want to generate hooks with smaller data to reduce
+ * the number of renders.
+ * @returns {[data: CompressedData | null, error: ErrorData | null, componentId: string]}
+ * - Based on your notification function in the subscriber you will receive the data and error in the hook.
  */
-function useGeneric<Data, ErrorData> (
+function useGeneric<Data, ErrorData, CompressedData> (
   parameter: string,
   subscriberPool: Record<string, Subscriber<Data, ErrorData> | undefined>,
-  SubcriberClass: new (parameters: string) => Subscriber<Data, ErrorData>
-): [data: Data | null, error: ErrorData | null, componentId: string] {
+  SubcriberClass: new (parameters: string) => Subscriber<Data, ErrorData>,
+  stateCompresser: (data: Data | null) => CompressedData | null
+): [data: CompressedData | null, error: ErrorData | null, componentId: string] {
   const componentId = useId()
-  const [genericData, setGenericData] = useState<Data | null>(null)
-  const [errorResponse, setErrorResponse] = useState<ErrorData | null>(null)
+
+  // We can optimize the useState function combine both CompressedData and ErrorData in one state, this wil reduce the
+  // number of useState calls thus reducing the number of renders
+  const [state, setState] =
+    useState<{ data: CompressedData | null, error: ErrorData | null }>({ data: null, error: null })
+
+  const genericData = state.data
+  const errorResponse = state.error
 
   useEffect(() => {
     let mounted = true
@@ -75,8 +99,23 @@ function useGeneric<Data, ErrorData> (
           return
         }
 
-        setGenericData(data)
-        setErrorResponse(errorData)
+        if (data !== null && errorData !== null) {
+          console.warn(
+            'You are receiving data and errorData at the same time, this strongly discouraged and can introduce' +
+            ' unexpected behavior in your component, please check your notification function implementation'
+          )
+        }
+
+        let compressedData: CompressedData | null = null
+        try {
+          compressedData = stateCompresser(data)
+        } catch (e) {
+          console.error(`stateCompresser function failed with parameters: ${parameter}.` +
+            'Components using this parameter will have their data set to null')
+          console.error(e)
+        }
+
+        setState({ data: compressedData, error: errorData })
       })
     }
 
@@ -89,24 +128,35 @@ function useGeneric<Data, ErrorData> (
   return [genericData, errorResponse, componentId]
 }
 
+interface GeneratorResponse<Data, ErrorData> {
+  hook: (parameters: string) => [data: Data | null, error: ErrorData | null, componentId: string]
+  stateCompresser: <CompressedData>(stateCompresser: (data: Data | null) => CompressedData | null) =>
+  (parameters: string) => [data: CompressedData | null, error: ErrorData | null, componentId: string]
+}
+
 /**
  * Function to implement a hook in the most generic way possible, you need to implement the
  * fetchData and stopFetching functions. This hook will manage the state of the component based
  * on the callbacks implemented in the fetchData function, and must stop fetching when the
  * stopFetching function is called.
- * @param fetchData This function must fetch the data and call the notifyObservers function.
- * parameters is the unique id for the data you are fetching, and notifyObservers is a function
- * that you must call with the data and/or errorData you want to send to the component.
- * @param stopFetching This will be called when the last componnent using the generated hook parameter
- * is unmounted, you must stop fetching the data in this function.
- * @returns The generated hook
+ * @template Data the type of the data that will be handled.
+ * @template ErrorData the type of the error data that will be handled.
+ *
+ * @param {(
+ * parameters: string,
+ * notifyObservers: (data: Data | null, errorData: ErrorData | null) => void) => Promise<void>} fetchData
+ * - This function must fetch the data and call the notifyObservers function.
+ * @param {(parameters: string) => Promise<void>} stopFetching
+ * - This will be called when the last component using the generated hook parameter is unmounted,
+ * you must stop fetching the data in this function.
+ * @returns {GeneratorResponse<Data, ErrorData>} - The generated hook.
  */
 export function generateHook<Data, ErrorData> (
   fetchData: (
     parameters: string,
     notifyObservers: (data: Data | null, errorData: ErrorData | null) => void) => Promise<void>,
   stopFetching: (parameters: string) => Promise<void>
-): (parameters: string) => [data: Data | null, error: ErrorData | null, componentId: string] {
+): GeneratorResponse<Data, ErrorData> {
   // Implements subscriber class for this hook
   class HookSubscriber extends Subscriber<Data, ErrorData> {
     async fetchData (
@@ -124,8 +174,15 @@ export function generateHook<Data, ErrorData> (
   const subscriberPool: Record<string, HookSubscriber | undefined> = {}
 
   // Return hook function ommiting subscriber
-  return (parameters: string) => {
-    return useGeneric<Data, ErrorData>(parameters, subscriberPool, HookSubscriber)
+  return {
+    hook: (parameters: string) => {
+      return useGeneric<Data, ErrorData, Data>(parameters, subscriberPool, HookSubscriber, (data) => data)
+    },
+    stateCompresser: <CompressedData>(stateCompresser: (data: Data | null) => CompressedData | null) => {
+      return (parameters: string) => {
+        return useGeneric<Data, ErrorData, CompressedData>(parameters, subscriberPool, HookSubscriber, stateCompresser)
+      }
+    }
   }
 }
 
@@ -134,19 +191,24 @@ export function generateHook<Data, ErrorData> (
  * need to implement the fetchData function that will be called periodically and the interval.
  * Remember that fetchData is an async function, so you can use await inside it while the data
  * is being fetched. The fetchData function must call the notifyObservers function with the data
- * and/or errorData you want to send to the component.
+ * or errorData you want to send to the component.
  * The fetchData function wil stop being called when the last component using the generated hook
  * parameter is unmounted.
- * @param fetchData This function must fetch the data and call the notifyObservers function.
- * @param interval Interval in milliseconds between each fetchData call.
- * @returns The generated hook
+ * @template Data the type of the data that will be handled.
+ * @template ErrorData the type of the error data that will be handled.
+ *
+ * @param {(parameters: string, notifyObservers:
+ * (data: Data | null, errorData: ErrorData | null) => void) => Promise<void>} fetchData
+ * - This function must fetch the data and call the notifyObservers function.
+ * @param {number} interval - Interval in milliseconds between each fetchData call.
+ * @returns {GeneratorResponse<Data, ErrorData>} - The generated hook.
  */
 export function generatePeriodicHook<Data, ErrorData> (
   fetchData: (
     parameters: string,
     notifyObservers: (data: Data | null, errorData: ErrorData | null) => void) => Promise<void>,
   interval: number
-): (parameters: string) => [data: Data | null, error: ErrorData | null, componentId: string] {
+): GeneratorResponse<Data, ErrorData> {
   // Implements subscriber class for this hook
   class HookSubscriber extends Subscriber<Data, ErrorData> {
     poolingPid: number | undefined = undefined
@@ -175,26 +237,37 @@ export function generatePeriodicHook<Data, ErrorData> (
   const subscriberPool: Record<string, HookSubscriber | undefined> = {}
 
   // Return hook function ommiting subscriber
-  return (parameters: string) => {
-    return useGeneric<Data, ErrorData>(parameters, subscriberPool, HookSubscriber)
+  return {
+    hook: (parameters: string) => {
+      return useGeneric<Data, ErrorData, Data>(parameters, subscriberPool, HookSubscriber, (data) => data)
+    },
+    stateCompresser: <CompressedData>(stateCompresser: (data: Data | null) => CompressedData | null) => {
+      return (parameters: string) => {
+        return useGeneric<Data, ErrorData, CompressedData>(parameters, subscriberPool, HookSubscriber, stateCompresser)
+      }
+    }
   }
 }
 
 /**
  * This functiojn generates a hook that fetches data from a rails actioncable channel, you need
  * to implement the dataParser function that will parse the data received from the channel and
- * return the data and/or errorData you want to send to the component.
+ * return the data or errorData you want to send to the component.
  * Remember that ActionCable is not included in this library, you need to include it in your
  * project before using this hook or make sure the actioncable variable is defined before generating
  * the hook.
- * @param channelName Name of the channel you want to connect to.
- * @param dataParser Function that will parse the data received from the channel and return the
- * @returns The generated hook
+ * @template Data the type of the data that will be handled.
+ * @template ErrorData the type of the error data that will be handled.
+ *
+ * @param {string} channelName - Name of the channel you want to connect to.
+ * @param {(data: unknown) => [data: Data | null, error: ErrorData | null]} dataParser
+ * - Function that will parse the data received from the channel and return the parsed data or error data.
+ * @returns {GeneratorResponse<Data, ErrorData>} - The generated hook.
  */
 export function actionCableHook<Data, ErrorData> (
   channelName: string,
   dataParser: (data: unknown) => [data: Data | null, error: ErrorData | null]
-): (parameters: string) => [data: Data | null, error: ErrorData | null, componentId: string] {
+): GeneratorResponse<Data, ErrorData> {
   const ActionCable = window.ActionCable
 
   if (ActionCable === undefined) {
@@ -251,8 +324,14 @@ export function actionCableHook<Data, ErrorData> (
 
   const subscriberPool: Record<string, HookSubscriber | undefined> = {}
 
-  // Return hook function ommiting subscriber
-  return (parameters: string) => {
-    return useGeneric<Data, ErrorData>(parameters, subscriberPool, HookSubscriber)
+  return {
+    hook: (parameters: string) => {
+      return useGeneric<Data, ErrorData, Data>(parameters, subscriberPool, HookSubscriber, (data) => data)
+    },
+    stateCompresser: <CompressedData>(stateCompresser: (data: Data | null) => CompressedData | null) => {
+      return (parameters: string) => {
+        return useGeneric<Data, ErrorData, CompressedData>(parameters, subscriberPool, HookSubscriber, stateCompresser)
+      }
+    }
   }
 }
